df4f438c06e795870a8c15dec083cf7ed2671d3b
Sunilganwane <sunilganwane@example.com>
2022-04-06 00:11:50 +0530

    sunil

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
-// import React from 'react';
-import { Link } from "react-router-dom";
-import {NavLink} from 'react-router-dom';
+import React from 'react';
+// import { Link } from "react-router-dom";
+// import {NavLink} from 'react-router-dom';
 // // import Sidebar from './Components/Sidebar';
 
 function Header() {
@@ -13,21 +13,21 @@ function Header() {
                <div className="row align-items-center">
                   <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-6">
                      <div className="logo">
-                        <Link to='/' className='homelogo'>
+                        <a href='#' className='homelogo'>
                         <img src={process.env.PUBLIC_URL + '/assets/images/logo.png'} alt="logo" width='100px' /> 
                         <img src={process.env.PUBLIC_URL + '/assets/images/wlogo.png'} alt="logo" width='100px' /> 
-                        </Link>
+                        </a>
                       </div>
                   </div>
                   <div className="col-lg-8 d-none d-lg-block text-end">
                      <div className="main-menu">
                         <nav id="mobile-menu">
                            <ul>
-                              <li><NavLink  activeclassname="active" to='/about'>About</NavLink ></li>
-                              <li><NavLink  to='/blogs'>Blog</NavLink ></li>
-                              <li><NavLink  to='/packages'>Packages</NavLink ></li>
-                              <li><NavLink  to='/contact'>Contact Us</NavLink ></li>
-                              <li><NavLink  to='/login'>Login / Sign up</NavLink ></li>
+                              <li><a href='#'>About</a></li>
+                              <li><a href='#'>Blog</a></li>
+                              <li><a href='#'>Packages</a></li>
+                              <li><a href='#'>Contact Us</a></li>
+                              <li><a href='#'>Login / Sign up</a></li>
                               <li><div className="language">
                                  <select>
                                     <option>ENG</option>
@@ -36,19 +36,7 @@ function Header() {
                               </div>
                               </li>
                     
-                             {/* <!--  <li className="has-dropdown">
-                                 <a href="about.html">Pages</a>
-                                 <ul className="submenu">
-                                    <li><a href="faq.html">Faq</a></li>
-                                    <li><a href="portfolio.html">Portfolio</a></li>
-                                    <li><a href="portfolio-details.html">Portfolio Details</a></li>
-                                    <li><a href="team.html">Team</a></li>
-                                    <li><a href="team-details.html">Team Details</a></li>
-                                    <li><a href="error.html">Error 404</a></li>
-                                    <li><a href="sign-up.html">Sign Up</a></li>
-                                    <li><a href="sign-in.html">Sign In</a></li>
-                                 </ul>
-                              </li>  --> */}
+                             
                               
                            </ul>
                         </nav>
