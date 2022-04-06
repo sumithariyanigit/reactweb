2d6945a7af8a5afa11397773ce7b1e3ca90f5d85
Sunilganwane <sunilganwane@example.com>
2022-04-06 12:46:18 +0530

    sunil

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 import HeroSection from './Components/HeroSection'
 import FeaturedCategory from './Components/FeaturedCategory';
+import PopularCategory from './Components/PopularCategory';
+import BestOffers from './Components/BestOffers';
+import RecentBlogs from './Components/RecentBlogs';
+import Subscribe from './Components/Subscribe';
+
 
 function App() {
   return (
     <div className="App">
     <HeroSection />
     <FeaturedCategory />
+    <PopularCategory />
+    <BestOffers />
+    <RecentBlogs />
+
+    <Subscribe />
     </div>
   );
 }
-
 export default App;
