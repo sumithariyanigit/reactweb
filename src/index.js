64b9b0ec18f65fa839d2347825a39efed8bf31eb
Sunilganwane <sunilganwane@example.com>
2022-04-06 14:47:51 +0530

    testing

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
-// import Header from './Header'
-// import Footer from './Footer'
+import Header from './Header'
+import Footer from './Footer'
 import About from './About'
 
 
@@ -11,10 +11,12 @@ import About from './About'
 const Routing = () => {
   return(
     <Router>
+      <Header />
       <Switch>
         <Route exact path="/home" component={App} />
         <Route exact path="/about" component={About} />
       </Switch>
+      <Footer />
     </Router>
   )
 }
