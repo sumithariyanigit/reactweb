d7a6fe78701afc2f3edc5352520e7c95a49b9d4d
Sunilganwane <sunilganwane@example.com>
2022-04-06 19:11:29 +0530

    push code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter } from "react-router-dom"
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import Header from './Header'
+import Footer from './Footer'
 import About from './About'
 import Blogs from "./Blogs";
 import Packages from "./Packages";
@@ -22,17 +23,11 @@ import Terms from "./Terms";
 
 const Routing = () => {
   return(
-    <>
-  
-    </>
-  )
-}
-
-
-ReactDOM.render(
-  <BrowserRouter>
+    <Router>
+      <Header />
       <Switch>
         <Route exact path="/" component={App} />
+        <Route exact path="/home" component={App} />
         <Route exact path="/about" component={About} />
         <Route exact path="/blogs" component={Blogs} />
         <Route exact path="/packages" component={Packages} />
@@ -47,7 +42,17 @@ ReactDOM.render(
         <Route exact path="/changepassword" component={ChangePassword} />
         <Route exact path="/privacy" component={Privacy} />
         <Route exact path="/terms" component={Terms} />
+
       </Switch>
-  </BrowserRouter>,
+      <Footer />
+    </Router>
+  )
+}
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Routing />
+  </React.StrictMode>,
   document.getElementById('root')
 );
