8628040f4620eba27620ffceb3384ab16e2406d3
Sunilganwane <sunilganwane@example.com>
2022-04-15 16:24:16 +0530

    git push

--- src/Components/FeaturedCategory.js
+++ src/Components/FeaturedCategory.js
@@ -2,6 +2,7 @@ import React from "react" ;
 import{useState,useEffect } from 'react';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function FeaturedCategory() {
 
